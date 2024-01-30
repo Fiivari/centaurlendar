defaaa0483e7d318bf5ddaccf80ce8f60783d5cf
Fiivari <126698151+Fiivari@users.noreply.github.com>
2024-01-30 09:17:26 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
+const basicAuth = require('basic-auth');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const adminCredentials = { 
+    username: process.env.ADMIN_USERNAME, 
+    password: process.env.ADMIN_PASSWORD 
+};
+
+const authMiddleware = (req, res, next) => {
+    const user = basicAuth(req);
+  
+    if (!user || user.name !== adminCredentials.username || user.pass !== adminCredentials.password) {
+      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+      return res.status(401).send('Unauthorized');
+    }
+  
+    return next();
+  };
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+const availableTimeSlotsFile = path.join(__dirname, 'availableTimeSlots.json');
+let availableTimeSlots = {};
+try {
+    if (fs.existsSync(availableTimeSlotsFile)) {
+        const fileContent = fs.readFileSync(availableTimeSlotsFile, 'utf-8');
+
+        // Check if the file is not empty
+        if (fileContent.trim() !== '') {
+            availableTimeSlots = JSON.parse(fileContent);
+        }
+    }
+} catch (error) {
+    console.error('Error reading or parsing the file:', error.message);
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index', 'index.html'));
+});
+
+app.post('/submit-booking', function (req, res) {
+    const selectedDate = req.body.selectedDate;
+    const selectedTime = req.body.selectedTime;
+    const userEmail = req.body.email;
+    
+    const bookedTime = parseFloat(selectedTime);
+    const bookedDate = availableTimeSlots[selectedDate];
+
+    if (bookedDate) {
+        const index = bookedDate.indexOf(bookedTime);
+        bookedDate.splice(index, 1);
+        if (bookedDate.length === 0) {
+            availableTimeSlots[selectedDate] = [];
+        }
+
+        // Update the availableTimeSlots in the file
+        fs.writeFileSync(availableTimeSlotsFile, JSON.stringify(availableTimeSlots));
+    }
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: 'info@niocell.com',
+        subject: 'Centaurlendar Ajanvaraus',
+        text: `Palaveri ajalle ${selectedDate} ${selectedTime} varattu! varaaja ${userEmail}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+          //res.status(500).json({ success: false, message: 'Error sending email' });
+        } else {
+          console.log('Email sent: ' + info.response);
+          //res.json({ success: true, message: `Palaveri ajalle ${selectedDate} ${selectedTime} varattu!` });
+        }
+    });
+
+    // Process the bookingDate and perform necessary actions
+    res.send(`Palaveri ajalle ${selectedDate} ${selectedTime} varattu!`);
+});
+
+app.use('/admin', authMiddleware);
+
+app.get('/admin', authMiddleware, function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'admin.html'));
+});
+
+app.post('/update-time-slots', function (req, res) {
+    const selectedDate = req.body.selectedDate;
+    const timeSlots = req.body.timeSlots.split(',').map(Number);
+
+    // Update availableTimeSlots for the selected date
+    availableTimeSlots[selectedDate] = timeSlots;
+
+    try {
+        // Write updated availableTimeSlots to the file
+        fs.writeFileSync(availableTimeSlotsFile, JSON.stringify(availableTimeSlots));
+        res.send(`Time slots for ${selectedDate} updated: ${timeSlots}`);
+    } catch (error) {
+        console.error('Error writing to the file:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/get-available-time-slots', function (req, res) {
+    // Read the JSON file
+    fs.readFile(availableTimeSlotsFile, 'utf8', function (err, data) {
+        if (err) {
+            console.error('Error reading availableTimeSlots.json:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        try {
+            // Parse the JSON data
+            const availableTimeSlots = JSON.parse(data);
+
+            // Log the contents to the console
+            console.log('availableTimeSlots:', availableTimeSlots);
+
+            // Send the data to the client
+            res.json(availableTimeSlots);
+        } catch (parseError) {
+            console.error('Error parsing availableTimeSlots.json:', parseError);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+});
+
+const server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
---
+++ public/index/index.js
@@ -0,0 +1,166 @@
+function updateCalendar() {
+    const selectedMonth = document.getElementById('monthSelector').value;
+    const selectedYear = document.getElementById('yearSelector').value;
+    const calendarDiv = document.getElementById('calendar');
+
+    // Fetch availableTimeSlots data from the server
+    fetch('/get-available-time-slots')
+        .then(response => response.json())
+        .then(data => {
+            const availableTimeSlots = data;
+
+            const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+            const firstDayOfMonth = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+
+            let calendarHTML = '<table>';
+            calendarHTML += '<tr><th>Ma</th><th>Ti</th><th>Ke</th><th>To</th><th>Pe</th><th>La</th><th>Su</th></tr>';
+
+            let dayCounter = 1;
+            let weekCounter = 0;
+
+            while (dayCounter <= daysInMonth) {
+                calendarHTML += '<tr>';
+                for (let j = 1; j < 8; j++) {
+                    const currentDate = new Date();
+                    const isPastDay = new Date(selectedYear, selectedMonth - 1, dayCounter + 1) < currentDate;
+
+                    if ((weekCounter === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
+                        calendarHTML += `<td class="${isPastDay ? 'past-date' : ''} empty-cell"></td>`;
+                    } else {
+                        const formattedDate = `${selectedYear}-${selectedMonth.toString().padStart(2, '0')}-${dayCounter.toString().padStart(2, '0')}`;
+
+                        const hasAvailableTimeSlots = availableTimeSlots[formattedDate] && availableTimeSlots[formattedDate].length > 0;
+
+                        calendarHTML += `<td class="${isPastDay ? 'past-date' : ''} ${hasAvailableTimeSlots ? 'has-times' : ''}" data-day="${dayCounter}" onclick="selectDate(${dayCounter})">${dayCounter}</td>`;
+                        dayCounter++;
+                    }
+                }
+                calendarHTML += '</tr>';
+                weekCounter++;
+            }
+
+            calendarHTML += '</table>';
+            calendarDiv.innerHTML = calendarHTML;
+
+
+            // Update time slots based on the received data
+            generateTimeSlots(availableTimeSlots);
+        })
+        .catch(error => {
+            console.error('Error fetching availableTimeSlots:', error);
+        });
+}
+
+function selectDate(day) {
+    const selectedDateInput = document.getElementById('selectedDate');
+    const selectedMonth = document.getElementById('monthSelector').value;
+    const selectedYear = document.getElementById('yearSelector').value;
+    const bottomCalendar = document.querySelector('.bottom-calendar');
+
+    // Format the date as needed
+    const formattedDate = `${selectedYear}-${selectedMonth.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    selectedDateInput.value = formattedDate;
+
+    const dateCells = document.querySelectorAll('#calendar td');
+    dateCells.forEach(cell => cell.classList.remove('selected-date'));
+
+    // Find the selected date cell and add the "selected-date" class
+    const selectedDateCell = document.querySelector(`#calendar td[data-day="${day}"]`);
+    if (selectedDateCell) {
+        selectedDateCell.classList.add('selected-date');
+    }
+
+    // Make the bottom part visible
+    bottomCalendar.style.display = 'block';
+
+    selectTimeSlot('');
+
+    // Fetch availableTimeSlots data from the server and update time slots
+    fetch('/get-available-time-slots')
+        .then(response => response.json())
+        .then(data => {
+            generateTimeSlots(data);
+        })
+        .catch(error => {
+            console.error('Error fetching availableTimeSlots:', error);
+        });
+}
+
+function generateTimeSlots(availableTimeSlots) {
+    const timeSlotsDiv = document.getElementById('timeSlots');
+    const selectedDate = document.getElementById('selectedDate').value;
+
+    // Check if available time slots are defined for the selected date
+    const dayTimeSlots = (availableTimeSlots && availableTimeSlots[selectedDate]) || [];
+
+    let timeSlotsHTML = '<h2>Valitse aika:</h2>';
+    if (dayTimeSlots.length === 0) {
+        // If no time slots are set, disable the submit button
+        document.querySelector('form input[type="submit"]').disabled = true;
+        timeSlotsHTML += `Ei aikoja tälle päivälle`;
+    } else {
+        // If time slots are set, enable the submit button
+        document.querySelector('form input[type="submit"]').disabled = false;
+    }
+
+    for (const timeSlot of dayTimeSlots) {
+        timeSlotsHTML += `<div class="time-slot bookable" onclick="selectTimeSlot(${timeSlot})">${formatTime(timeSlot)}</div>`;
+    }
+
+    timeSlotsDiv.innerHTML = timeSlotsHTML;
+}
+
+function selectTimeSlot(hour) {
+    const selectedTimeInput = document.getElementById('selectedTime');
+    selectedTimeInput.value = formatTime(hour);
+}
+
+function formatTime(hour) {
+    if (hour === '') {
+        return ''
+    }
+    const formattedHour = Math.floor(hour);
+    const formattedMinutes = (hour % 1 === 0.5) ? '30' : '00';
+
+    const nextHour = (formattedHour + (formattedMinutes === '30' ? 1 : 0)).toString().padStart(2, '0');
+    const nextMinutes = (formattedMinutes === '30' ? '00' : '30');
+
+    return `${formattedHour.toString().padStart(2, '0')}:${formattedMinutes} - ${nextHour}:${nextMinutes}`;
+}
+
+// Set default values and update calendar on page load
+document.addEventListener('DOMContentLoaded', function () {
+    const currentDate = new Date();
+    const defaultMonth = currentDate.getMonth() + 1; // Months are zero-based
+    const defaultYear = currentDate.getFullYear();
+
+    document.getElementById('monthSelector').value = defaultMonth.toString();
+    document.getElementById('yearSelector').value = defaultYear.toString();
+
+    updateCalendar();
+});
+
+// Submit success actions
+document.querySelector('#bookingForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    const selectedDate = document.querySelector('#selectedDate').value;
+    const selectedTime = document.querySelector('#selectedTime');
+    const userEmail = document.querySelector('#email').value;
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/submit-booking');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            console.log(xhr.responseText);
+            selectedTime.value = 'Aika varattu. Otamme yhteyttä sähköpostilla';
+        }
+        else {
+            console.error('Error fetching availableTimeSlots:', xhr.statusText);
+        }
+    };
+    xhr.onerror = function() {
+        console.error('Error fetching availableTimeSlots:', xhr.statusText);
+    };
+    xhr.send(`selectedDate=${selectedDate}&selectedTime=${selectedTime.value}&email=${userEmail}`);
+});
+
